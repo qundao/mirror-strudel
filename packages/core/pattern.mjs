971cfe15fc25bf9df080a62b63e5403e1956824e
Aria <weglossing@gmail.com>
2025-11-18 16:07:22 -0600

    Merge main and use parray

--- packages/core/pattern.mjs
+++ packages/core/pattern.mjs
@@ -3552,29 +3552,6 @@ export const morph = (frompat, topat, bypat) => {
   return frompat.innerBind((from) => topat.innerBind((to) => bypat.innerBind((by) => _morph(from, to, by))));
 };
 
-const _asArrayPattern = (pats) => {
-  const pack = (...xs) => xs;
-  let acc = pure(curry(pack, null, pats.length));
-  for (const p of pats) acc = acc.appBoth(p);
-  return acc;
-};
-
-/**
- * Establishes an FX chain. Can be called by chaining .FX(<fx1>).FX(<fx2>)..
- * calls and/or in a single .FX(<fx1>, <fx2>, ..) call. The <fx1>, .. are _patterns_ which
- * establish the controls of the given effect. See examples.
- * @name FX
- * @memberof Pattern
- * @returns Pattern
- */
-Pattern.prototype.FX = function (...effects) {
-  effects = effects.map(reify);
-  return this.withValue((v) => (vEff) => {
-    const currFX = v.FX ?? [];
-    return { ...v, FX: currFX.concat(vEff) };
-  }).appLeft(_asArrayPattern(effects));
-};
-
 /**
  * Soft-clipping distortion
  *
@@ -3709,4 +3686,20 @@ Pattern.prototype.phases = function (list) {
 // Also create a top-level function
 export const phases = (list) => {
   return _ensureListPattern(list).as('phases');
+};
+
+/**
+ * Establishes an FX chain. Can be called by chaining .FX(<fx1>).FX(<fx2>)..
+ * calls and/or in a single .FX(<fx1>, <fx2>, ..) call. The <fx1>, .. are _patterns_ which
+ * establish the controls of the given effect. See examples.
+ * @name FX
+ * @memberof Pattern
+ * @returns Pattern
+ */
+Pattern.prototype.FX = function (...effects) {
+  effects = effects.map(reify);
+  return this.withValue((v) => (vEff) => {
+    const currFX = v.FX ?? [];
+    return { ...v, FX: currFX.concat(vEff) };
+  }).appLeft(parray(effects));
 };
